ff775ba39f000c99d6a4e1aa8ea1d4ba398d2354
unknown <arafa1002@gmail.com>
2025-08-12 08:27:08 -0300

    aula01-revisao

--- aula0.py
+++ aula0.py
@@ -1,1 +1,19 @@
-print ("hello world")
+x = float(input())
+
+#faz uma condicional da nota
+if x > 7:
+    print("Você passou!")
+else:
+    print("Você é burro")
+
+
+x = "lucro"
+y = 1345980.19
+z = 4
+
+lista = [x,y,z]
+
+dicionario = {"Objetivo": x,
+              "Total": y,
+              "Culturas": z}
+
